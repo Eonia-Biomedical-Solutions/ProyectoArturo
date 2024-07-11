f278c29603f207e94cb6337c3c414fc847a18d86
Turris20 <148021540+Turris20@users.noreply.github.com>
2024-07-11 16:10:55 -0600

    Update AdminBook.js

--- frontend/src/app/components/AdminBook.js
+++ frontend/src/app/components/AdminBook.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import styles from './page.module.css'
+import { IoCartOutline, IoSearchCircle } from "react-icons/io5";
+import { TbEdit } from "react-icons/tb";
+import { FaRegTrashCan } from "react-icons/fa6";
 import { collection, getFirestore, deleteDoc, getDocs, doc, updateDoc } from 'firebase/firestore';
-import appFirebase from '@/credenciales'; // Replace with your Firebase configuration
+import appFirebase from '@/credenciales';
 import { getAuth } from 'firebase/auth';
 
 const auth = getAuth(appFirebase);
@@ -15,14 +19,16 @@ const AdminBook = () => {
         ISBN: '',
         genero: '',
         fechaPublicacion: '',
-        resumen: ''
+        resumen: '',
+        imagen: '',
+        precio: '',
     });
 
-    const db = getFirestore(appFirebase); // Get Firestore instance
+    const db = getFirestore(appFirebase);
 
     useEffect(() => {
         fetchLibros();
-    }, []); // Run only once on component mount
+    }, []);
 
     const fetchLibros = async () => {
         try {
@@ -56,7 +62,9 @@ const AdminBook = () => {
             ISBN: libro.ISBN,
             genero: libro.genero,
             fechaPublicacion: libro.fechaPublicacion,
-            resumen: libro.resumen
+            resumen: libro.resumen,
+            imagen: libro.imagen,
+            precio: libro.precio,
         });
     };
 
@@ -72,7 +80,9 @@ const AdminBook = () => {
                 ISBN: '',
                 genero: '',
                 fechaPublicacion: '',
-                resumen: ''
+                resumen: '',
+                imagen: '',
+                precio: '',
             });
         } catch (error) {
             console.error('Error updating libro:', error);
@@ -81,66 +91,113 @@ const AdminBook = () => {
 
     return (
         <div className="App">
-            <h1>Libros</h1>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Buscar por título..."
-                    value={filtroTitulo}
-                    onChange={(e) => setFiltroTitulo(e.target.value)}
-                />
-                <ul>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <h1 style={{fontFamily:'monospace', color:'white', marginBlock: '20px'}}>Libros</h1>
+                <div style={{ margin: '44px', display: 'flex', flexDirection: 'row', backgroundColor: 'white', borderRadius: '20px' }}>
+                    <IoSearchCircle style={{ fontSize: '30px', color: 'rgb(11, 11, 11)' }} />
+                    <input
+                        type="text"
+                        placeholder="Buscar por título..."
+                        value={filtroTitulo}
+                        onChange={(e) => setFiltroTitulo(e.target.value)}
+                        style={{ paddingLeft: '20px', border: 'none', borderRadius: '20px', width: '70vh' }} />
+                </div>
+                <ul className={styles.bookList}>
                     {libros
                         .filter((libro) =>
                             libro.titulo.toLowerCase().includes(filtroTitulo.toLowerCase())
                         )
                         .map((libro) => (
-                            <li key={libro.id}>
+                            <li key={libro.id} className={styles.bookItem}>
                                 {editandoLibro === libro.id ? (
-                                    <div>
+                                    <div className={styles.EditBook}>
+                                        <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>URL de imagen:</p>
                                         <input
                                             type="text"
-                                            value={nuevoLibro.titulo}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, titulo: e.target.value })}
-                                            placeholder="Título"
+                                            value={nuevoLibro.imagen}
+                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, imagen: e.target.value })}
+                                            placeholder="URL de imagen"
                                         />
-                                        <input
-                                            type="text"
-                                            value={nuevoLibro.autor}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, autor: e.target.value })}
-                                            placeholder="Autor"
-                                        />
-                                        <input
-                                            type="text"
-                                            value={nuevoLibro.ISBN}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, ISBN: e.target.value })}
-                                            placeholder="ISBN"
-                                        />
-                                        <input
-                                            type="text"
-                                            value={nuevoLibro.genero}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, genero: e.target.value })}
-                                            placeholder="Género"
-                                        />
-                                        <input
-                                            type="text"
-                                            value={nuevoLibro.fechaPublicacion}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, fechaPublicacion: e.target.value })}
-                                            placeholder="Fecha de Publicación"
-                                        />
-                                        <input
-                                            type="text"
-                                            value={nuevoLibro.resumen}
-                                            onChange={(e) => setNuevoLibro({ ...nuevoLibro, resumen: e.target.value })}
-                                            placeholder="Resumen"
-                                        />
-                                        <button onClick={() => saveLibro(libro.id)}>Guardar</button>
+                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', width: '100%' }}>
+                                            <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Título:</p>
+                                            <input
+                                                type="text"
+                                                value={nuevoLibro.titulo}
+                                                onChange={(e) => setNuevoLibro({ ...nuevoLibro, titulo: e.target.value })}
+                                                placeholder="Título" />
+
+                                            <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Autor:</p>
+                                            <input
+                                                type="text"
+                                                value={nuevoLibro.autor}
+                                                onChange={(e) => setNuevoLibro({ ...nuevoLibro, autor: e.target.value })}
+                                                placeholder="Autor" />
+
+                                            <div style={{ display: 'flex', flexDirection: 'row', marginTop: '5px' }}>
+                                                <div style={{ width: '65%' }}>
+                                                    <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Resumen:</p>
+                                                    <textarea
+                                                        type="text"
+                                                        value={nuevoLibro.resumen}
+                                                        onChange={(e) => setNuevoLibro({ ...nuevoLibro, resumen: e.target.value })}
+                                                        placeholder="Resumen"
+                                                        style={{ height: '90%' }} />
+                                                </div>
+                                                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '15px', width: '12vh' }}>
+                                                    <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Género:</p>
+                                                    <input
+                                                        type="text"
+                                                        value={nuevoLibro.genero}
+                                                        onChange={(e) => setNuevoLibro({ ...nuevoLibro, genero: e.target.value })}
+                                                        placeholder="Género"
+                                                    />
+                                                    <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Fecha de publicación:</p>
+                                                    <input
+                                                        type="text"
+                                                        value={nuevoLibro.fechaPublicacion}
+                                                        onChange={(e) => setNuevoLibro({ ...nuevoLibro, fechaPublicacion: e.target.value })}
+                                                        placeholder="Fecha de Publicación"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>Precio:</p>
+                                            <input
+                                                type="text"
+                                                value={nuevoLibro.precio}
+                                                onChange={(e) => setNuevoLibro({ ...nuevoLibro, precio: e.target.value })}
+                                                placeholder="Precio"
+                                            />
+                                            <p style={{ color: 'rgb(122, 122, 122)', marginBottom: '0', marginTop: '10px', fontSize: '12px' }}>ISBN:</p>
+                                            <input
+                                                type="text"
+                                                value={nuevoLibro.ISBN}
+                                                onChange={(e) => setNuevoLibro({ ...nuevoLibro, ISBN: e.target.value })}
+                                                placeholder="ISBN"
+                                            />
+                                        </div>
+                                        <button className={styles.btnlibro1} onClick={() => saveLibro(libro.id)}>Guardar</button>
                                     </div>
                                 ) : (
-                                    <div>
-                                        <strong>{libro.titulo}</strong> - {libro.autor} ({libro.fechaPublicacion}) [{libro.genero}] - ISBN: {libro.ISBN} - Resumen: {libro.resumen}
-                                        <button onClick={() => editLibro(libro)}>Editar</button>
-                                        <button onClick={() => deleteLibro(libro.id)}>Eliminar</button>
+                                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                        <img src={libro.imagen} className={styles.imagenlibro} />
+                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', width: '100%' }}>
+                                            <p className={styles.librotitulo}>{libro.titulo}</p>
+                                            <p className={styles.librotexto1}>{libro.autor}</p>
+                                            <div style={{ display: 'flex', flexDirection: 'row', marginTop: '5px' }}>
+                                                <div style={{ width: '65%' }}>
+                                                    <p className={styles.librotexto2}>{libro.resumen}</p>
+                                                </div>
+                                                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '15px', width: '12vh' }}>
+                                                    <p className={styles.librotexto2}>{libro.genero}</p>
+                                                    <p className={styles.librotexto2}>{libro.fechaPublicacion}</p>
+                                                </div>
+                                            </div>
+                                            <p className={styles.librotexto1}>${libro.precio}</p>
+                                        </div>
+                                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                            <button style={{ border: '0', marginRight: '15px' }} onClick={() => editLibro(libro)}><TbEdit style={{ fontSize: '24px', color: 'blue' }} /></button>
+                                            <button style={{ border: '0', marginLeft: '15px' }} onClick={() => deleteLibro(libro.id)}><FaRegTrashCan style={{ fontSize: '20px', color: 'red' }} /></button>
+                                        </div>
                                     </div>
                                 )}
                             </li>
